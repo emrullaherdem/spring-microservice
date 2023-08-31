8866ac82fd73aa8c525f4bbaaa54a1faace8c696
Emrullah Erdem <emrullah.erdem@outlook.com>
2023-08-31 06:22:21 +0300

    product-service mvc build added Request POJO for Product

---
+++ product-service/src/main/java/com/springmicroservice/productservice/controller/ProductController.java
@@ -0,0 +1,20 @@
+package com.springmicroservice.productservice.controller;
+
+import com.springmicroservice.productservice.dto.ProductRequest;
+import com.springmicroservice.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/product")
+public class ProductController {
+
+    private final ProductService productService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest){
+        productService.createProduct(productRequest);
+    }
+}
---
+++ product-service/src/main/java/com/springmicroservice/productservice/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.springmicroservice.productservice.repository;
+
+import com.springmicroservice.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product,String> {
+}
